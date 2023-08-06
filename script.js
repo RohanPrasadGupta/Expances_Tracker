0eb60b3b812aeb146f2cfa5b3d0af8371cba5e4d
ROHAN PRASAD GUPTA <rohanprasadgupta4@gmail.com>
2023-08-06 11:58:52 +0700

    ok

--- script.js
+++ script.js
@@ -31,52 +31,65 @@ let IncomeAmt = 0
 function onAddData(){
     const option1 = document.getElementById('option1');
     const option2 = document.getElementById('option2');
-    if(option1.checked){
-        CurAmout = amount.value
-        CurDesc =  desc.value
 
-        let li = document.createElement('li')
-        li.innerHTML = `${CurDesc} : ${CurAmout}`
-        li.style.borderColor='red'
-        listUl.appendChild(li)
-
-        Balance = Number(Balance) - Number(CurAmout)
-        BalanceData.innerHTML = `Balance: $ ${Balance}`
-
-        ExpanceAmt = Number(ExpanceAmt) + Number(CurAmout)
-        ExpanseAmout.innerHTML = `$${ExpanceAmt}`
-
-        amount.value=''
-        desc.value = ''
-        option1.checked = false
-        
-        
-        
+    if(amount.value===''){
+        alert("Plese enter the amout to Proceed...")
     }
-    else if(option2.checked){
-        CurAmout = amount.value
-        CurDesc =  desc.value
-        
-        let li = document.createElement('li')
-        li.innerHTML = `${CurDesc} : ${CurAmout}`
-        li.style.borderBlockColor='green'
-        listUl.appendChild(li)
-        
-        Balance = Number(Balance) + Number(CurAmout)
-        BalanceData.innerHTML = `Balance: $ ${Balance}`
-        
-        IncomeAmt = Number(IncomeAmt) + Number(CurAmout)
-        IncomeAmount.innerHTML = `$${IncomeAmt}`
-        amount.value=''
-        desc.value = ''
-        option2.checked = false
-        
-
-        
+    else if(desc.value===''){
+        alert("Plese enter Description of the amount...")
     }
+
     else{
-        alert('Pick any Catagory to add')
+        if(option1.checked){
+            CurAmout = amount.value
+            CurDesc =  desc.value
+    
+            let li = document.createElement('li')
+            li.innerHTML = `${CurDesc} : ${CurAmout}`
+            li.style.borderColor='red'
+            listUl.appendChild(li)
+    
+            Balance = Number(Balance) - Number(CurAmout)
+            BalanceData.innerHTML = `Balance: $ ${Balance}`
+    
+            ExpanceAmt = Number(ExpanceAmt) + Number(CurAmout)
+            ExpanseAmout.innerHTML = `$${ExpanceAmt}`
+    
+            amount.value=''
+            desc.value = ''
+            option1.checked = false
+            
+            
+            
+        }
+        else if(option2.checked){
+            CurAmout = amount.value
+            CurDesc =  desc.value
+            
+            let li = document.createElement('li')
+            li.innerHTML = `${CurDesc} : ${CurAmout}`
+            li.style.borderBlockColor='green'
+            listUl.appendChild(li)
+            
+            Balance = Number(Balance) + Number(CurAmout)
+            BalanceData.innerHTML = `Balance: $ ${Balance}`
+            
+            IncomeAmt = Number(IncomeAmt) + Number(CurAmout)
+            IncomeAmount.innerHTML = `$${IncomeAmt}`
+            amount.value=''
+            desc.value = ''
+            option2.checked = false
+            
+    
+            
+        }
+        else{
+            alert('Pick any Catagory to add')
+        }
+
     }
+
+    
 }
 
 
